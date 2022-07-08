3c3c0162c0654691ea9be9fc7f749004604c0ebb
cagatayy <cagatay.yagmur@infinidium.com.tr>
2022-07-08 10:54:37 +0300

    random color initilize

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
-let css = document.querySelector("h3")
-let color1 = document.querySelector(".color1")
-let color2 = document.querySelector(".color2")
-let body = document.getElementById("gradient")
+let css = document.querySelector("h3");
+let color1 = document.querySelector(".color1");
+let color2 = document.querySelector(".color2");
+let body = document.getElementById("gradient");
 var btn = document.getElementById("random");
 
 
 //i set a colors' value in style.css file.
 
-color1.setAttribute("value", "#7bfc03") 
-color2.setAttribute("value", "#943246")
+color1.setAttribute("value", getRandomColor()) 
+color2.setAttribute("value", getRandomColor())
 
-
+body.style.background = "linear-gradient(to right, "+ color1.value + ", "+ color2.value +")";
+css.textContent = body.style.background + ";"
 
 let event1 = function(){
     body.style.background = "linear-gradient(to right, "+ color1.value + ", "+ color2.value +")";
